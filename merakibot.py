3ee3c43adfd1290aa642ed48adfac470d7d4e4c3
billgrant <bill@billgrant.io>
2018-06-15 22:07:11 -0400

    Bot working pre meraki code

--- merakibot.py
+++ merakibot.py
@@ -4,7 +4,7 @@ import re
 from slackclient import SlackClient
 
 # instantiate Slack client
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(os.environ.get('MERAKI_BOT_TOKEN'))
 
 # merakibot's user ID in Slack: vlaue is assigned after the bot starts.
 merakibot_id = None
@@ -12,7 +12,7 @@ merakibot_id = None
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
-MENTION_REGEX = "^<@(|[WU].+?>(.*)"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_bot_commands(slack_events):
     """Parses a list of events coming from the Slack RTM API to find
@@ -23,7 +23,7 @@ def parse_bot_commands(slack_events):
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
-            if user_id, message == merakibot_id:
+            if user_id == merakibot_id:
                 return message, event["channel"]
     return None, None
 
@@ -35,15 +35,13 @@ def parse_direct_mention(message_text):
     matches = re.search(MENTION_REGEX, message_text)
     """The first group contains the username, the second group contains the
     remaining message"""
-    return (matches.group(1), matches.group(2).strip()) if matches else
-        (None, None)
-
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+ 
 def handle_command(command, channel):
     """Executes bot command if the command is known"""
 
     # Default response is help text for the user
-    default_response = 
-    "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
     # Finds and executes the given command, filling in response
     response = None
@@ -61,11 +59,11 @@ def handle_command(command, channel):
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
+        print("Meraki Bot connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         merakibot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
             command, channel = parse_bot_commands(slack_client.rtm_read())
-
             if command:
                 handle_command(command, channel)
             time.sleep(RTM_READ_DELAY)
